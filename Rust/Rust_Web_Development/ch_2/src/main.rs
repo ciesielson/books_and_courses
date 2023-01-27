de3eb113d95f4f05f15977cd40c0b5a6908a40d6
ciesielson <ciesielczyk.bartosz@hotmail.com>
2023-01-27 22:55:16 +0100

    Rust Programming Language kickoff

---
+++ Rust/Rust_Web_Development/ch_2/src/main.rs
@@ -0,0 +1,24 @@
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags
+        }
+    }
+}
+
+fn main() {
+    let question = Question::new("1", "First Question", "Content of question", ["faq"]);
+    println!("{}", question)
+}
