120802a7a4ad4423255974fa920d6682d649bc1a
ciesielson <ciesielczyk.bartosz@hotmail.com>
2023-03-15 14:34:48 +0100

    002 fibonacci #3 recursive solution (very slow)

--- Rust/The_Rust_Programming_Language/ch_3/nth_fibonacci/src/main.rs
+++ Rust/The_Rust_Programming_Language/ch_3/nth_fibonacci/src/main.rs
@@ -1,38 +1,47 @@
 fn main() {
-    fibonacci_iterative(14)
+    let result = slowFib(50);
+    println!("the result is... {}", result)
 }
 
-fn generate_nth_fibonacci_number(n: i32) {
-    if n == 0 {
-        println!("Generated number: {}", n);
-    } else if n == 1 {
-        println!("Generated number: {}", n);
-    } else if n > 1 {
-        let mut f_prim = 0;
-        let mut f = 1;
-        let mut generated_number: i32 = 0;
-
-        for _i in 2..=n {
-            generated_number = f + f_prim;
-            f_prim = f;
-            f = generated_number;
-        }
-
-        println!("Generated number: {}", generated_number);
-    } else {
-        println!("Only positive numbers are allowed");
-    }
-}
-
-fn fibonacci_iterative(n: i32) {
-    let mut result = vec![0, 1];
-
-    for _i in 2..=n {
-        let a = result[result.len() - 1];
-        let b = result[result.len() - 2];
-
-        result.push(a + b)
+fn slowFib(n: i32) -> i32 {
+    if n < 2 {
+        return n;
     }
 
-    println!("The nth number is... {}", result[result.len() - 1])
+    slowFib(n - 2) + slowFib(n - 1)
 }
+
+// fn generate_nth_fibonacci_number(n: i32) {
+//     if n == 0 {
+//         println!("Generated number: {}", n);
+//     } else if n == 1 {
+//         println!("Generated number: {}", n);
+//     } else if n > 1 {
+//         let mut f_prim = 0;
+//         let mut f = 1;
+//         let mut generated_number: i32 = 0;
+//
+//         for _i in 2..=n {
+//             generated_number = f + f_prim;
+//             f_prim = f;
+//             f = generated_number;
+//         }
+//
+//         println!("Generated number: {}", generated_number);
+//     } else {
+//         println!("Only positive numbers are allowed");
+//     }
+// }
+
+// fn fibonacci_iterative(n: i32) {
+//     let mut result = vec![0, 1];
+//
+//     for _i in 2..=n {
+//         let a = result[result.len() - 1];
+//         let b = result[result.len() - 2];
+//
+//         result.push(a + b)
+//     }
+//
+//     println!("The nth number is... {}", result[result.len() - 1])
+// }
